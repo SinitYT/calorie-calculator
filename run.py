b89fc2aac441de35e08c0531e7ca1afd58d33f83
SinitYT <sinityonas@outlook.com>
2022-08-08 11:47:24 +0000

    Initial commit, added the first functions that calculates the resting metabolic rate

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def rmr_calculator():
+    sex = input("Please enter your gender:\n")
+    age = input("Please enter your age:\n")
+    weight = input("Please enter your weight in kgs:\n")
+    height = input("Please enter your height in cms:\n")
+    rmr = 0
+    
+    if sex == 'female':
+        rmr= 9.99 * int(weight)+ 6.25 * int(height) - 4.92 * int(age) + 161
+    else:
+         rmr = 9.99 * int(weight) + 6.25 * int(height) - 4.92 * int(age) + 5
+
+    print(f"Your RMR is {rmr}")
+
+
+rmr_calculator()
+
+   
+    
+
+
+ 
