3208570a04c8a26d9a5f0dc99e8eb6d16d32de3a
SinitYT <sinityonas@outlook.com>
2022-08-15 20:34:43 +0000

    Add a function that gives user the option of losing or gaining weight

--- run.py
+++ run.py
@@ -16,13 +16,13 @@ def dataCollection():
     weight = getInput(prompt="Enter your weight in kgs:\n",cast=float,condition=lambda x: x > 0,errorMessage="That's not a valid weight")
     height = getInput(prompt="Enter your height in cms:\n", cast=float, condition=lambda x: x > 100,errorMessage="That's not a valid height")
     activity = getInput(prompt="How active are you? Type in either sedentary,light,moderate,active,very active:\n")
-    weight_loss = getInput(prompt="How many kgs do you want to lose?\n",cast=float)
+    
     data.append(sex)
     data.append(age)
     data.append(weight)
     data.append(height)
     data.append(activity)
-    data.append(weight_loss)
+    
     return data
 
 def calories_cal(data):
@@ -31,7 +31,7 @@ def calories_cal(data):
     weight = data[2]
     height = data[3]
     activity = data[4]
-    weight_loss = data[5]
+    
 
     if data[0] == 'male' or 'm':
         rmr= 10 * int(weight)+ 6.25 * int(height) - 5 * int(age) + 5
@@ -57,10 +57,45 @@ def calories_cal(data):
 
    
     tee = tef + adl 
-    print(tee)
+    print(f"Your estimated daily calorie needs to maintain your current weight is {tee}kcal\n")
+    return tee
+
+'''Give options to user wether they want to lose weight, gain weight or how long i will take them to reach their goal'''
+def choices():
+    done = False
+    while not done:
+        print("""
+        (1)Lose weight
+        (2)Gain Weight
+        (3)How long will it take me to lose weight
+        """)
+        choice = input("Choose an option: \n")
+        data = dataCollection()
+        current_cal_intake=calories_cal(data) 
+        if choice == '1' :
+            loss_goal=int(input("Enter how many kgs you would like to lose?\n"))
+
+            print("Calculating your calorie deficit...\n")
+            new_cal_intake = round(current_cal_intake - (current_cal_intake * 0.25))
+            calorie_def_week = (((current_cal_intake * 0.25) * 7) * 0.45) / 3500    #weight lost in kgs
+            days_it_takes = round((loss_goal * 7) / calorie_def_week)
+            user_answer = print(f"You need to consume {new_cal_intake}kcal to lose {loss_goal} kgs and it will take you {days_it_takes}days.\n")
+
+        
+        elif choice == '2':
+            gain_goal=int(input("Enter how many kgs you would like to gain?/n"))
+            print("Calculating your calorie increase...\n")
+            new_cal_intake = round(current_cal_intake + 1000)
+            days_it_takes = round((gain_goal*7)/0.9 )
+            user_answer = print(f"You need to consume {new_cal_intake}kcal to gain {gain_goal} kgs and it will take you {days_it_takes}days.\n")
+            
+
+        
+        print(user_answer)
+print(choices())
 
 
 
 
-data = dataCollection()
-calories_cal(data)
+
+
